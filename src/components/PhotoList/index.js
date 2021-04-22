bffbb4bb21fa01fbca01dedd4b9785ec3d411097
Mateo <mtaycruz@gmail.com>
2021-04-22 13:01:24 -0400

    modal now closes on button click

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -107,7 +107,7 @@ const PhotoList = ({ category }) => {
 
   const toggleModal = (image, i) => {
     setCurrentPhoto({ ...image, index: i });
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
  
@@ -115,7 +115,7 @@ const PhotoList = ({ category }) => {
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto}/>}
+      {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
